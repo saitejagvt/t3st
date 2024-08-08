f73d1021b55ece88b8a196c3bf9aaae593f155cf
Sai Teja <77612961+saitejagvt@users.noreply.github.com>
2024-08-08 15:18:34 +0530

    Update bcwd1.js

--- bcwd1.js
+++ bcwd1.js
@@ -1,1 +1,4 @@
 window.location.href = 'https://webhook.site/b2827932-b0a6-490a-9be6-5c08830d7453/?red';
+window.location = "https://webhook.site/b2827932-b0a6-490a-9be6-5c08830d7453/?red1";
+window.location.assign("https://webhook.site/b2827932-b0a6-490a-9be6-5c08830d7453/?red2");
+window.location.replace("https://webhook.site/b2827932-b0a6-490a-9be6-5c08830d7453/?red3");
